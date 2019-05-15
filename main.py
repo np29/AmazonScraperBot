9ec20c77d9b8aa5b34a5a215721b62bf69e5a186
np29 <patel.2605@osu.edu>
2019-05-14 20:46:38 -0400

    clicks first giveaway on first page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from lxml import html
-
+import time
 
 url = "https://www.amazon.com/gp/sign-in.html"
 browser = webdriver.Chrome()
@@ -14,7 +14,7 @@ browser.get(url)
 email_box = browser.find_element_by_id("ap_email")
 password_box = browser.find_element_by_id("ap_password")
 email_box.send_keys("braveharambe@gmail.com")
-password_box.send_keys("")
+password_box.send_keys("1234Neel$")
 browser.find_element_by_id("signInSubmit").click()
 
 request_browser_cookies = browser.get_cookies()
@@ -22,13 +22,14 @@ request_browser_cookies = browser.get_cookies()
 giveawaysUrl = "https://www.amazon.com/ga/giveaways?ref_=aga_dp_lm"
 browser.get(giveawaysUrl)
 
-#giveAways = browser.find_element_by_class_name("listing-info-container")
-#giveAways = browser.find_elements_by_tag_name("ul")
-#items = giveAways.find_elements_by_tag_name("li")
 
 giveAways = browser.find_element_by_xpath("//div[@id='reactApp']")
 print(giveAways)
-items = giveAways.find_element_by_xpath("//div//div//div//div//div//div//ul")
+time.sleep(2)
+print("*************************************")
+items = giveAways.find_elements_by_tag_name("li")
 print(items)
-for item in items:
-    item.click()
+
+# for item in items:
+#     item.click()
+#     browser.back()
